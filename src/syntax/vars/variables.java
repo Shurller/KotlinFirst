241e04e60db75342fee9ba45036fec842e2ac8c0
shurl <shurller@mail.ru>
2025-05-05 07:27:35 +0200

    pack

---
+++ src/syntax/vars/variables.java
@@ -0,0 +1,36 @@
+package syntax.vars;
+
+public class variables {
+    public static void main(String[] args) {
+
+        byte nnn = 127; //-128...127
+        System.out.println(Integer.parseInt("11111111", 2));
+        System.out.println("The byte number: "+nnn);
+
+
+        short nns = 32767; //-32768...32676
+        System.out.println("The short number: "+nns);
+
+        int nnw = 1400303030; //-2kkk...2kkk
+        System.out.println("The int number: "+nnw);
+
+        long nnq = 1_922_337_203_342L; //для наглядности
+        System.out.println("The long number: "+nnq);
+
+        float nnp = 3.14f;
+        System.out.println("The float number: "+nnp);
+
+        double nnr = 3.14;
+        System.out.println("The double number: "+nnr);
+        System.out.println(Double.isNaN(23/0.0));
+
+        char nnc = 'A';
+        System.out.println("The char symbol: "+nnc);
+
+        boolean nnb = true;
+        System.out.println("The boolean value: "+nnb);
+
+        var somevar = 10L;
+
+    }
+}
